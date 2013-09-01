85b105450194580f65245c82164ae582adfaf5bb
Brion Vibber <brion@pobox.com>
2013-09-01 14:03:18 -0700

    Initial stub host, loads en.wikipedia in a nonfunctional frame

--- HybridHost/ViewController.h
+++ HybridHost/ViewController.h
@@ -8,6 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIWebViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *menuButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *searchButton;
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *forwardButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *languageButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *actionButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *bookmarkButton;
+
+- (IBAction)menuButtonPushed:(id)sender;
+- (IBAction)searchButtonPushed:(id)sender;
+
+- (IBAction)backButtonPushed:(id)sender;
+- (IBAction)forwardButtonPushed:(id)sender;
+- (IBAction)languageButtonPushed:(id)sender;
+- (IBAction)actionButtonPushed:(id)sender;
+- (IBAction)bookmarkButtonPushed:(id)sender;
 
 @end
--- HybridHost/ViewController.m
+++ HybridHost/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self loadFirstPage];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +27,63 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark Navbar button handlers
+
+- (IBAction)menuButtonPushed:(id)sender {
+}
+
+- (IBAction)searchButtonPushed:(id)sender {
+}
+
+#pragma mark Toolbar button handlers
+
+- (IBAction)backButtonPushed:(id)sender {
+}
+
+- (IBAction)forwardButtonPushed:(id)sender {
+}
+
+- (IBAction)languageButtonPushed:(id)sender {
+}
+
+- (IBAction)actionButtonPushed:(id)sender {
+}
+
+- (IBAction)bookmarkButtonPushed:(id)sender {
+}
+
+#pragma mark UIWebViewDelegate methods
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    // FIXME check if we should pass off to an external browser
+    // or internal native module
+    return YES;
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    NSLog(@"webViewDidStartLoad");
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    NSLog(@"webViewDidFinishLoad");
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    NSLog(@"webView didFailLoadWithError: %@", error);
+}
+
+#pragma mark private methods
+
+- (void)loadFirstPage {
+    NSString *urlString = @"https://en.m.wikipedia.org/wiki/";
+    [self loadURL:[NSURL URLWithString:urlString]];
+}
+
+- (void)loadURL:(NSURL *)url {
+    [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+}
+
 
 @end
