2051a366cbf4e8e2fe656962d16920251d7892da
Brion Vibber <brion@pobox.com>
2013-09-01 13:08:28 -0700

    Initial Commit

---
+++ HybridHost/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HybridHost
+//
+//  Created by Brion on 9/1/13.
+//  Copyright (c) 2013 Brion Vibber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HybridHost/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HybridHost
+//
+//  Created by Brion on 9/1/13.
+//  Copyright (c) 2013 Brion Vibber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
